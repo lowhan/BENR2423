fdb46238e6b337cdf3288734fcac80f813f553c7
lowhan <b022010033@student.utem.edu.my>
2022-05-09 01:23:40 +0800

    Week7 - Exercise - TDD

--- Week7/user.js
+++ Week7/user.js
@@ -1,20 +1,19 @@
 let users;
-
-const { hash } = require("bcrypt");
+const { hash } = require("bcrypt");									//For password encryption
 const bcrypt = require("bcryptjs")
 const saltRounds = 10;
 var encrypt;
 
 class User {
 	static async injectDB(conn) {
-		users = await conn.db("TDDweek7").collection("users")
+		users = await conn.db("TDDweek7").collection("users")		//users = db.collection.
 	}
 
 	static async register(username, password) {
 		// TODO: Hash password
 		bcrypt.genSalt(saltRounds, function (saltError, salt) { 
 			if (saltError) {
-			  throw saltError
+			  	throw saltError
 			} else {
 			  	bcrypt.hash(password, salt, function(hashError, hash) {    //encrypt password
 					if (hashError) {
@@ -26,22 +25,22 @@ class User {
 			}
 		});
 		// TODO: Check if username exists
-		return users.findOne({								//find the document
+		return users.findOne({							//find the document
 			$and: [{ 
 				'username': username,				
 				'password': password
 			}]
 		}).then(async user =>{
 			if (user) {
-				if ( user.username == username )
+				if ( user.username == username )		//check whether the username exists or not
 				{
 					return "user exist";
 				}
 			}
 			else
 			{
-				// TODO: Save user to database
-				await users.insertOne({						//insert new document
+				// TODO: Save user to database			//if username is not exist, create new
+				await users.insertOne({					//insert new document
 					'username' : username,
 					'password' : password,
 					'encrypt': encrypt
@@ -53,32 +52,31 @@ class User {
 
 	static async login(username, password) {
 		// TODO: Check if username exists
-		return users.findOne({								//find the document
+		return users.findOne({															//find the document
 			$or: [
 				{'username': username},				
 				{'password': password}
 			]				
-		}).then(async user =>{
+		}).then(async user =>{		
 		// TODO: Validate password ( or username )
-			if (user) {
-				if ( user.username != username && user.password == password) {
+			if (user) {																	//if the username exists 
+				if ( user.username != username && user.password == password) {			//if the username is invalid
 					return "invalid username";
 				}
-				else if ( user.username == username && user.password != password ) {
+				else if ( user.username == username && user.password != password ) {	//else if the password is invalid
 					return "invalid password";
 				}
-				else
+				else																	//else username and password are valid
 				{
 					// TODO: Return user object
-					return user;
+					return user;	
 				}
 			}
-			else
+			else																		//else the username doesn't exists
 			{
 				return "There is not such document";
 			}
 		})
 	}
 }
-
 module.exports = User;
--- Week7/user.test.js
+++ Week7/user.test.js
@@ -1,32 +1,38 @@
 const MongoClient = require("mongodb").MongoClient;
 const User = require("./user")
 
-//sample data
+///////////////////////sample user (used to test)//////////////////////////////
+
 const username = "user1"																
 const password = "passwordfromuser1"
 const encrypt = "$2a$10$VjvtPgldEMsz5C9GUlyhDenl.Xc7gY82bvDUiCaEKUXUHFcL4cH4u"
 
+//////////////////////////////////////////////////////////////////////////////
+
 describe("User Account", () => {
 	let client;
 	beforeAll(async () => {
-		client = await MongoClient.connect(
+		client = await MongoClient.connect(		//connection to mongoDB
 			"mongodb+srv://m001-student:m001-mongodb-basics@Sandbox.vqzcw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
 			{ useNewUrlParser: true },
 		);
 		User.injectDB(client);
 	})
-
 	afterAll(async () => {
 		await client.close();
 	})
 
 //////////////////////////////////////////////////////////////////////////////
 
-	test("New user registration", async () => {
+	//if new user is created, this test should be pass
+	test("New user registration", async () => {				
 		const res = await User.register(username, password)
-		expect(res).toBe("new user created");
+		expect(res).toBe("new user created");		
 	})
 
+	//if duplicate user is found, this test should be pass
+	//#duplicate test might pass also when checking for registration, because the function is
+	//#executed twice and the insertion is made before checking for the duplicate
 	test("Duplicate username", async () => {
 		const res = await User.register(username, password)
 		expect(res).toBe("user exist");
@@ -34,27 +40,29 @@ describe("User Account", () => {
 
 //////////////////////////////////////////////////////////////////////////////
 
+	//if sample user's username and password doesn't match to any documents (aka the document doesn't exists)
 	test("Username doesn't exist to login", async () => {
 		const res = await User.login(username, password)
 		expect(res).toBe("There is not such document");
 	})
 
+	//if sample user's username doesn't match to any document's username
 	test("User login invalid username", async () => {
 		const res = await User.login(username, password)
 		expect(res).toBe("invalid username");
 	})
 
+	//if sample user's password doesn't match to any document's password
 	test("User login invalid password", async () => {
 		const res = await User.login(username, password)
 		expect(res).toBe("invalid password");
 	})
 
+	//if username and password are matched, check for the encrypt password before allow user to login
 	test("User login successfully", async () => {
 		const res = await User.login(username, password)
 		expect(res.username).toBe(username);
 		expect(res.password).toBe(password);
 		expect(res.encrypt).toBe(encrypt);
 	})
-
-//////////////////////////////////////////////////////////////////////////////
 });
